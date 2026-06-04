f3f45a98bf7e6ca61d7f816bd15e1f0ec6afc699
Tom Zhang <treez.zhang@gmail.com>
2026-06-03 21:36:27 -0700

    rm lint

--- MATH_157.lean
+++ MATH_157.lean
@@ -1,5 +1,5 @@
-import «MATH_157».BasicIdentities
-import «MATH_157».Backscatter
-import «MATH_157».RevisedModel
-import «MATH_157».RTE
-import «MATH_157».Wideband
+import MATH_157.Backscatter
+import MATH_157.BasicIdentities
+import MATH_157.RTE
+import MATH_157.RevisedModel
+import MATH_157.Wideband
